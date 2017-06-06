a8a40060c731f7d931885ff2043a0792768105bc
karamawariken <24karamawariken@gmail.com>
2017-06-06 23:03:06 +0900

    説明文追加 いらない素材削除

--- mane_zombi_ute/GameScene.swift
+++ mane_zombi_ute/GameScene.swift
@@ -19,30 +19,19 @@ class GameScene: SKScene {
     
     
     override func didMove(to view: SKView) {
-        
- 
+        //背景色
         self.backgroundColor = SKColor.green
-        
+        //キャラクターの位置を設定
         player.position = CGPoint(x: 0.0, y:size.height/3)
-        //skspritenodeの大きさを倍にする
-        player.setScale(3)
-        
         makura_left.position = CGPoint(x: -size.width/3, y:-size.height/3)
-        makura_left.setScale(6)
         makura_center.position = CGPoint(x: 0.0, y:-size.height/3)
-        makura_center.setScale(6)
         makura_right.position = CGPoint(x: size.width/3, y:-size.height/3)
-        makura_right.setScale(6)
-
-        //self.addChild(backNode)
+        
+        //SKSceneにキャラクターを入れる
         self.addChild(player)
         self.addChild(makura_left)
         self.addChild(makura_center)
         self.addChild(makura_right)
     }
-    
-    
-    
-    
     
 }
